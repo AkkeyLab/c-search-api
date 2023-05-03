e06aa21f8163bbb896b94ca1bd2d49ce4e012a45
AkkeyLab <17640999+AkkeyLab@users.noreply.github.com>
2023-05-03 21:04:29 +0900

    :sparkles: Write a handler to return all items

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type company struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
@@ -8,4 +14,15 @@ type company struct {
 var companies = []company{
 	{ID: "1", Name: "AkkeyLab, inc."},
 	{ID: "2", Name: "Apple, inc."},
+}
+
+func getCompanies(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, companies)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/companies", getCompanies)
+
+	router.Run("localhost:8080")
 }
